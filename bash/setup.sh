34e94d20f5418d5d281e6193b5ed2e83d33ad02a
Sam Meyer-Reed <samwisemr@gmail.com>
2017-09-15 17:20:06 -0400

    Add wallpaper setup

--- bash/setup.sh
+++ bash/setup.sh
@@ -5,7 +5,11 @@ HERE=$(dirname "${BASH_SOURCE[0]}")
 command cp $HERE/bashrc ~/.bashrc
 command cp $HERE/bash_prompt ~/.bash_prompt
 command cp $HERE/bash_aliases ~/.bash_aliases
+command cp $HERE/profile ~/.profile
 command cp $HERE/dope_commands ~/dope_commands
+
+command cp $HERE/fehbg ~/.fehbg
+command chmod a+x ~/.fehbg
 
 command mkdir -p ~/bin
 command cp $HERE/bashFunctions.sh ~/bin/bashFunctions.sh
