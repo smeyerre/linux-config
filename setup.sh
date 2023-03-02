ca4f8d3443aec0df26ecbac535af95866d3c66f2
Sam Meyer-Reed <samwisemr@gmail.com>
2023-03-02 07:28:37 -0500

    update tmux setup

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ HERE=$(dirname "${BASH_SOURCE[0]}")
 
 sudo apt-get install vim-gnome
 sudo apt-get install i3
-sudo apt-get install tmux
+#sudo apt-get install tmux
 
 "$HERE"/bash/setup.sh
 "$HERE"/i3/setup.sh
 "$HERE"/i3status/setup.sh
-"$HERE"/tmux/setup.sh
+#"$HERE"/tmux/setup.sh
 "$HERE"/vim/setup.sh
 
 echo "All set up!"
