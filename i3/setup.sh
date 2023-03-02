a1225f17d9e74c465c127babf5bd5acb29cbbae3
Sam Meyer-Reed <samwisemr@gmail.com>
2023-03-02 08:43:39 -0500

    add lockscreen and background

--- i3/setup.sh
+++ i3/setup.sh
@@ -7,5 +7,7 @@ command cp -r $HERE/bin ~/.config/i3/
 command cp -r $HERE/i3blocks/blocklets ~/.config/i3/
 command cp $HERE/fehbg ~/.fehbg
 command cp $HERE/i3blocks/i3blocks.conf /etc/i3blocks.conf
+command cp $HERE/windows-xp-bliss-4k-lu-2560x1440.jpg ~/Pictures
+command cp $HERE/lockscreen.png ~/Pictures
 
 echo "i3 files set up!"
