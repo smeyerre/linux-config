67ed65aea10cf78cdcdd3bc8a8ca12bafede13de
Sam Meyer-Reed <smeyerreed@gmail.com>
2023-03-14 13:35:04 +0100

    update other i3 files

--- i3/setup.sh
+++ i3/setup.sh
@@ -5,9 +5,7 @@ HERE=$(dirname "${BASH_SOURCE[0]}")
 command cp $HERE/config ~/.config/i3/config
 command cp -r $HERE/bin ~/.config/i3/
 command cp -r $HERE/i3blocks/blocklets ~/.config/i3/
-command cp $HERE/fehbg ~/.fehbg
 command cp $HERE/i3blocks/i3blocks.conf /etc/i3blocks.conf
-command cp $HERE/windows-xp-bliss-4k-lu-2560x1440.jpg ~/Pictures
-command cp $HERE/lockscreen.png ~/Pictures
+command cp $HERE/wallpaper.jpg $HERE/lockscreen.png ~/Pictures
 
 echo "i3 files set up!"
