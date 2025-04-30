9a4bd6b4ab790e31da8c818bed54141b9d9457f6
Sam Meyer-Reed <smeyerreed@gmail.com>
2025-04-30 12:17:50 -0400

    Fix bash commands file name

--- bash/setup.sh
+++ bash/setup.sh
@@ -6,7 +6,7 @@ command cp $HERE/bashrc ~/.bashrc
 command cp $HERE/bash_prompt ~/bash_prompt
 command cp $HERE/bash_aliases ~/.bash_aliases
 command cp $HERE/profile ~/.profile
-command cp $HERE/dope_commands ~/dope_commands
+command cp $HERE/commands ~/commands
 
 command mkdir -p ~/bin
 command cp $HERE/bin/* ~/bin/
